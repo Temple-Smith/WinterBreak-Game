e7b83196e64aa2cfadfcf289fa9d94e259320b99
Drew Temple-Smith <drewtemplesmith@gmail.com>
2025-12-28 11:20:07 -0700

    Increased money label font size and changed color to green

--- scripts/status_ui.gd
+++ scripts/status_ui.gd
@@ -6,4 +6,4 @@ func _ready() -> void:
 	assert(Autoload.has_method("get_player_money"), "Autoload should have function get_player_money")
 
 func _process(delta: float) -> void:
-	money_label.text = str(Autoload.get_player_money())
+	money_label.text = str(Autoload.get_player_money()) + " $"
