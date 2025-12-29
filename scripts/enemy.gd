8b2d20b6bdfbfc08ad87f897fd5702092b39ff4f
Drew Temple-Smith <drewtemplesmith@gmail.com>
2025-12-29 15:27:19 -0700

    Added enemy projectiles, player takes damage.

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,38 +1,32 @@
 class_name Enemy extends CharacterBody2D
 enum State { IDLE, AGGRO }
-var state: State = State.IDLE
+
+@export var projectile_scene: PackedScene = preload("res://scenes/player/projectile.tscn")
+@export var fire_rate: float = 2.0
+@export var attack_range: float = 250.0
 @export var speed := 10.0
 @export var max_health := 30
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var hp_fill: ColorRect = $HPBar/Fill
 
+var state: State = State.IDLE
 var current_hp: int = max_health
 var full_hp_width: float
-@onready var hp_fill: ColorRect = $HPBar/Fill
 var player: Node2D
 var is_dead := false
+var fire_cooldown: float = 0.0
+var fire_timer: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
 	current_hp = max_health
-	#player = get_tree().get_first_node_in_group("player")
+	player = get_tree().get_first_node_in_group("player")
 	sprite.play("idle_Left")
 	
 	full_hp_width = hp_fill.size.x
 	update_hp_bar()
-	
-#func _process(delta: float) -> void:
-	#if not player:
-		#return
-	#
-	#var direction :=(player.global_position - global_position).normalized()
-	#velocity = speed * direction
-	#
-	#move_and_slide()
-	#
-	#update_animation(direction)
-	#pass
 	
 func update_animation(direction: Vector2) -> void:
 	if abs(direction.x) > abs(direction.y):
@@ -44,7 +38,6 @@ func update_animation(direction: Vector2) -> void:
 		sprite.play("idle_Left")
 		return
 		
- 	
 func _physics_process(delta: float) -> void:
 	if is_dead:
 		return
@@ -58,9 +51,34 @@ func _physics_process(delta: float) -> void:
 			if player:
 				var direction: Vector2 = (player.global_position - global_position).normalized()
 				velocity = direction * speed
+				fire_timer -= delta
+				if fire_timer <= 0.0:
+					fire_at_player(direction)
+					fire_timer = fire_rate
+				
 				update_animation(direction)
 				
 	move_and_slide()
+
+func fire_at_player(direction: Vector2) -> void:
+	if projectile_scene == null:
+		return
+	
+	var spread_angle: float = 0.3
+	var bullets := 3
+	
+	for i in bullets:
+		# Calculate angle offset for each bullet
+		var t: float = float(i) / float(bullets - 1)  # 0..1
+		var angle_offset: float = lerp(-spread_angle, spread_angle, t)
+		var bullet_dir: Vector2 = direction.rotated(angle_offset).normalized()
+		var projectile: Projectile = projectile_scene.instantiate()
+		projectile.position = global_position
+		projectile.lifetime = 2.0
+		projectile.speed = 80
+		projectile.setup(bullet_dir, Projectile.Owner.ENEMY)
+		get_parent().add_child(projectile)
+ 
 	
 func take_damage(amount: int) -> void:
 	if is_dead:
@@ -92,6 +110,8 @@ func flash_red() -> void:
 func die() -> void:
 	is_dead = true
 	queue_free()
+	
+	
 
 
 func _on_aggro_range_body_entered(body: Node2D) -> void:
